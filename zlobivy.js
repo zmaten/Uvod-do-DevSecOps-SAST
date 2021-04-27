e27ecb5fc46c17e7f00016fc02afde4c2e07b9be
zmaten <kain@technet.ms>
2021-04-27 23:15:23 +0200

    Update zlobivy.js

--- zlobivy.js
+++ zlobivy.js
@@ -1,1 +1,4 @@
 const connectionString = "host=localhost;username=guest;password=heslo;virtualHost=RabbitMQTest";
+const  storageconn = "DefaultEndpointsProtocol=https;AccountName=demofilestoragesamples20;AccountKey=fw/Z6ou//OXhfIvwe8fZsHna3yJrW92iGOSJH/3hGkekhtxmg==;EndpointSuffix=core.windows.net";  
+  
+        
